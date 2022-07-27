6cd68850a4e3561ee16e60db853e412dd4035f08
koparkrea <kopakrea@naver.com>
2022-07-27 18:21:32 +0900

    FIRST

---
+++ compute_pp_pr.py
@@ -0,0 +1,100 @@
+import torch
+import numpy as np
+from sklearn.metrics import pairwise_distances
+
+
+def get_pairwise_distances(real, fake, jobs = 8, gpu = False):
+    if not gpu:
+        pairwise_distance_between_real = pairwise_distances(real, real, n_jobs = jobs, metric = 'l2')
+        pairwise_distance_between_fake = pairwise_distances(fake, fake, n_jobs = jobs, metric = 'l2')
+        pairwise_distance_between_real_fake = pairwise_distances(real, fake, n_jobs = jobs, metric = 'l2')
+        pairwise_distance_between_fake_real = np.transpose(pairwise_distance_between_real_fake)
+    else:
+        pairwise_distance_between_real = torch.cdist(real, real, p = 2)
+        pairwise_distance_between_fake = torch.cdist(fake, fake, p = 2)
+        pairwise_distance_between_real_fake = torch.cdist(real, fake, p = 2)
+        pairwise_distance_between_fake_real = torch.transpose(pairwise_distance_between_real_fake, 0, 1)
+    
+    return (pairwise_distance_between_real,
+           pairwise_distance_between_fake,
+           pairwise_distance_between_real_fake,
+           pairwise_distance_between_fake_real)
+
+def get_average_of_knn_distance(x, kth, gpu = False):
+    if not gpu:
+        indices = np.argpartition(x, kth + 1, axis = -1)[..., :kth+1]
+        k_smallests = np.take_along_axis(x, indices, axis = -1)
+        kth_values = k_smallests.max(axis = -1)
+        k_nearest = np.mean(kth_values)
+    else:
+        k_nearest = torch.mean(torch.sort(x, dim = 1)[0][:, kth])
+
+    return k_nearest
+
+def get_hypersphere_f(distance, k_nearest, a):
+    out_of_knearest = distance >= a * k_nearest
+    f = 1 - distance / (a * k_nearest)
+    f[out_of_knearest] = 0.0
+    return f
+
+def get_scoring_rule_psr(f, kth, gpu = False):
+    if not gpu:
+        f = np.sort(f, axis = 0)[-kth:]
+        PSR = np.prod(1.0 - f, axis = 0)
+    else:
+        f = torch.sort(f, dim = 0)[0][-kth:]
+        PSR = torch.prod(1.0 - f, dim = 0)
+    return PSR
+
+
+
+
+def pprecision_precall(real, fake, a = 1.5, kth = 4, gpu = False):
+    """
+    Main Calculation of P-precision and P-recall.
+    Args:
+        real : Embeddings of real sample (N X D)
+        fake : Embeddings of fake sample (N X D)
+        a : Hyperparameter which controls the size of hypersphere
+        kth : Hyperparameter for KNN
+    Retures:
+        p-precision, p-recall 
+    """
+    if gpu:
+        print('Calculate with GPU')
+        real, fake = torch.Tensor(real).cuda(), torch.Tensor(fake).cuda()
+
+    pairwise_distance_real, pairwise_distance_fake, distance, distance_t = get_pairwise_distances(real, fake, gpu = gpu)
+
+    # Calculate maximum distance for f
+    k_nearest_x = get_average_of_knn_distance(pairwise_distance_real, kth, gpu = gpu)
+    k_nearest_y = get_average_of_knn_distance(pairwise_distance_fake, kth, gpu = gpu)
+    
+    # Derive hypersphere f
+    f_x = get_hypersphere_f(distance, k_nearest_x, a)
+    f_y = get_hypersphere_f(distance_t, k_nearest_y, a)
+    
+    # Estimate mass function of distribution
+    PSR_X = get_scoring_rule_psr(f_x, kth, gpu = gpu)
+    PSR_Y = get_scoring_rule_psr(f_y, kth, gpu = gpu)
+    
+    # Calculate the expectation of mass
+    if gpu:
+        p_precision = torch.mean(1.0 - PSR_X).cpu()
+        p_recall = torch.mean(1.0 - PSR_Y).cpu()
+    else:
+        p_precision = np.mean(1.0 - PSR_X)
+        p_recall = np.mean(1.0 - PSR_Y)
+    
+    return p_precision, p_recall
+
+    
+    
+
+
+
+        
+
+                                                                           
+
+
---
+++ toy_experiment.py
@@ -0,0 +1,134 @@
+import os
+import torch
+import random
+import argparse
+import numpy as np
+from compute_pp_pr import pprecision_precall
+from prdc import compute_prdc
+
+def random_seed(seed):
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    torch.backends.cudnn.benchmark = True
+    torch.backends.cudnn.deterministic = True
+    np.random.seed(seed)
+    random.seed(seed)
+
+def get_dataset(dim = 64, datanum = 20000, u = 0, v = 1):
+    real = np.random.randn(datanum, dim)
+    fake = torch.empty((datanum, dim))
+    torch.nn.init.normal_(fake, u, v)
+    fake = fake.numpy()
+
+    return real, fake
+
+def main(args):
+    """
+    Toy-experiment (Gaussian Distribution) for testing accuracy and precision of measurement of evaluation metric
+    args:
+        setting:
+            outlier_f : Investigate the behavior of metric when there exist outlier in real distribution
+            outlier_d : Investigate the behavior of metric when there exist outlier in fake distribution
+            trade_off : Investigate the behavior of metric when fidelity and diversity trade-off
+            datanum : Investigate the behavior of metric for varying number of dataset
+            hyperparameter : Investigate the behavior of metric for varying hyperparameters
+        enable_gpu: Enable GPU calculation of PP&PR (default : False). Use for fast calculation with
+                     small dataset size, e.g., 10k, due to memory of GPU.
+    
+    Original Code for IP&IR and D&C is available at https://github.com/clovaai/generative-evaluation-prdc
+    """
+    
+    random_seed(args.seed)
+    
+    if args.gpu_enable:
+        os.environ['CUDA_VISIBLE_DEVICES'] = args.ngpu
+        #device = 'cuda'
+    
+    if args.setting == 'outlier_f':
+        outlier = -2 + np.random.randn(1, args.dim)
+        for sh in np.arange(-3.0, 3.2, 0.2):
+            print('Shifting Factor : ', sh)
+            real, fake = get_dataset(args.dim, args.datanum, u = sh)
+            real[0] = outlier
+
+            # PP&PR
+            p_precision, p_recall = pprecision_precall(real, fake, a = args.scale, kth = args.kth, gpu = args.gpu_enable)
+            print('p_precision : {:.5f}, \t p_recall : {:.5f}'.format(p_precision, p_recall))
+
+            #IP&IR / D&C
+            score = compute_prdc(real, fake, args.kth)
+            print(score)
+
+    elif args.setting == 'outlier_d':
+        outlier = 2 + np.random.randn(1, args.dim)
+        for sh in np.arange(-3.0, 3.2, 0.2):
+            print('Shifting Factor : ', sh)
+            fake, real = get_dataset(args.dim, args.datanum, u = sh)
+            fake[0] = outlier
+
+            # PP&PR
+            p_precision, p_recall = pprecision_precall(real, fake, a = args.scale, kth = args.kth, gpu = args.gpu_enable)
+            print('p_precision : {:.5f}, \t p_recall : {:.5f}'.format(p_precision, p_recall))
+
+            #IP&IR / D&C
+            score = compute_prdc(real, fake, args.kth)
+            print(score)
+
+    elif args.setting == 'trade_off':
+        for v in np.arange(0.2, 1.6, 0.1):
+            print('Variance : ', v)
+            real, fake = get_dataset(args.dim, args.datanum, v = v)
+
+            # PP&PR
+            p_precision, p_recall = pprecision_precall(real, fake, a = args.scale, kth = args.kth, gpu = args.gpu_enable)
+            print('p_precision : {:.5f}, \t p_recall : {:.5f}'.format(p_precision, p_recall))
+
+            #IP&IR / D&C
+            score = compute_prdc(real, fake, args.kth)
+            print(score)
+
+    elif args.setting == 'datanum':
+        num_list = [100, 500, 750, 1000, 2500, 5000, 7500, 10000, 15000, 20000, 30000, 40000, 50000]
+        for num in num_list:
+            print('Number of dataset : ', num)
+            real, fake = get_dataset(args.dim, num)
+
+            # PP&PR
+            p_precision, p_recall = pprecision_precall(real, fake, a = args.scale, kth = args.kth, gpu = args.gpu_enable)
+            print('p_precision : {:.5f}, \t p_recall : {:.5f}'.format(p_precision, p_recall))
+
+            #IP&IR / D&C
+            score = compute_prdc(real, fake, args.kth)
+            print(score)
+            
+    elif args.setting == 'hyperparameter':
+        a_list = [1.2, 1.5, 1.7, 2.0]
+        k_list = [2, 3, 4, 5, 6]
+        for a in a_list:
+            print('a : ', a)
+            for k in k_list:
+                print('k : ', k)
+                real, fake = get_dataset(args.dim, args.datanum)
+                p_precision, p_recall = pprecision_precall(real, fake, a = a, kth = k, gpu = args.gpu_enable)
+                lower_bound = 1 - (1 / a)**k
+                print('p_precision : {:.5f}, \t p_recall : {:.5f}, \t Lower Bound : {:.5f}'.format(p_precision, 
+                                                                                                p_recall, lower_bound))
+
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Toy-experiment with Evaluation Metrics')
+    parser.add_argument('--seed', type = int, default = 777, help = 'Fixing the seed')
+    parser.add_argument('--dim', type = int, default = 64, help = 'Dimension for feature embedding')
+    parser.add_argument('--datanum', type = int, default = 50000, help = 'Number of dataset')
+    parser.add_argument('--kth', type = int, default = 4, help = 'hyperparameter for KNN')
+    parser.add_argument('--scale', type = int, default= 1.5, help = 'hyperparameter for hypersphere f')
+    parser.add_argument('--setting', type = str, default= 'outlier_f', help = 'settings for toy-experiment')
+    parser.add_argument('--gpu_enable', action = 'store_true', default= False, help = 'Enable for GPU calculation (default = False)')
+    parser.add_argument('--ngpu', type = str, default = '0', help = 'GPU device number')
+    args = parser.parse_args()
+
+    main(args)
+
+        
